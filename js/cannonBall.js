58d4ea6548edc25a9afb7d85925bf32190e77346
menegoi <liviamenegoi@gmail.com>
2022-05-11 17:55:28 -0300

    first commit

---
+++ js/cannonBall.js
@@ -0,0 +1,34 @@
+class CannonBall{
+    constructor(x,y,r){
+
+        this.r = r;
+        this.image = loadImage("assets/cannonBall.png");
+
+        var options = {
+            isStatic:true
+        }
+        this.body = Bodies.circle(x,y,r, options);
+        World.add(world,this.body);
+
+    }
+    shoot(){
+        var newAngle = cannon.angle -28;
+        newAngle = newAngle*(3.14/180); 
+
+        var velocity = p5.Vector.fromAngle(newAngle);
+        velocity.mult(0.5);
+
+        Matter.Body.setStatic(this.body,false);
+        Matter.Body.setVelocity(this.body, {x: velocity.x*(180/3.14),y:velocity.y*(180/3.14)});
+    }
+
+    display(){
+        var position = this.body.position;
+
+        push();
+        image(this.image, position.x,position.y, this.r,this.r);
+        pop();
+
+        
+    }
+}
